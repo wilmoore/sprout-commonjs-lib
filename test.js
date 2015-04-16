084ee5be64967e5cfa4577258ab6d633ce59a7bb
Wil Moore III <wil.moore@wilmoore.com>
2015-04-16 03:34:31 -0600

    add tests

--- test.js
+++ test.js
@@ -8,8 +8,9 @@ var fs = require('fs')
 var path = require('path')
 var rimraf = require('rimraf')
 var Sprout = require('sprout')
-var sprout = new Sprout(path.resolve(__dirname, 'config'))
+var sprout = new Sprout(path.resolve(__dirname, 'test', 'config'))
 var test = require('tape-catch')
+var format = require('util').format
 
 /*!
  * imports (local).
@@ -18,7 +19,7 @@ var test = require('tape-catch')
 var locals_path = path.resolve(__dirname, 'locals.json')
 var locals = require(locals_path)
 var template_path = path.resolve(__dirname)
-var temporary_dir = path.resolve(__dirname, 'tmp')
+var test_project_path = path.resolve(__dirname, 'test', 'out')
 
 /*!
  * options.
@@ -34,7 +35,8 @@ var options = {
 
 var parameters = [
   { file: 'changelog.md', pattern: /[\d]{4}-[\d]{2}-[\d]{2}/ },
-  { file: 'index.js', pattern: locals.package_description }
+  { file: 'index.js', pattern: locals.package_description },
+  { file: 'package.json', pattern: format('"name": "%s"', locals.package_name) }
 ]
 
 /*!
@@ -45,17 +47,18 @@ test('sprout-commonjs-lib', function (t) {
   t.plan(parameters.length)
 
   sprout
-    .add('commonjs-lib', __dirname)
+    .add('commonjs-lib', template_path)
     .then(function () {
-      rimraf.sync(temporary_dir)
+      rimraf.sync(test_project_path)
     })
     .then(function () {
-      sprout.init('commonjs-lib', temporary_dir, options)
+      sprout.init('commonjs-lib', test_project_path, options).then(function () {
+        parameters.forEach(function (p) {
+          var file = path.resolve(test_project_path, p.file)
+          t.assert(fs.readFileSync(file, 'utf8').match(p.pattern), p.file)
+        })
+      })
     })
     .done(function () {
-      parameters.forEach(function (p) {
-        var file = path.resolve(temporary_dir, p.file)
-        t.assert(fs.readFileSync(file, 'utf8').match(p.pattern), p.file)
-      })
     })
 })
