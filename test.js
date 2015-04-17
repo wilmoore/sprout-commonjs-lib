b4d54c836f1f3dc6a6c93a4c7e991cdaa3ce1fc0
Wil Moore III <wil.moore@wilmoore.com>
2015-04-16 21:58:12 -0600

    Test Refactor w/ Parameterize Tests.

--- test.js
+++ test.js
@@ -4,29 +4,43 @@
  * imports.
  */
 
-var fs = require('fs')
-var path = require('path')
-var rimraf = require('rimraf')
-var Sprout = require('sprout')
-var sprout = new Sprout(path.resolve(__dirname, 'test', 'config'))
-var test = require('tape-catch')
 var format = require('util').format
+var fs = require('fs')
+var mkdirp = require('mkdirp').sync
+var path = require('path')
+var rimraf = require('rimraf').sync
+var Sprout = require('sprout')
+var test = require('tape-catch')
+
+/*!
+ * constants.
+ */
+
+var TEMPLATE_NAME = 'commonjs-lib'
+var BASE_OUT_PATH = path.resolve(process.env.TMPDIR, TEMPLATE_NAME)
+console.error(BASE_OUT_PATH)
+
+/*!
+ * paths.
+ */
+
+var projectCreationDir = path.resolve(BASE_OUT_PATH, 'out')
+var templateInstallDir = path.resolve(BASE_OUT_PATH, 'config')
+var templateLocalsPath = path.resolve(__dirname, 'locals.json')
+var templateSourceRoot = path.resolve(__dirname)
 
 /*!
  * imports (local).
  */
 
-var locals_path = path.resolve(__dirname, 'locals.json')
-var locals = require(locals_path)
-var template_path = path.resolve(__dirname)
-var test_project_path = path.resolve(__dirname, 'test', 'out')
+var locals = require(templateLocalsPath)
 
 /*!
  * options.
  */
 
 var options = {
-  config: locals_path
+  config: templateLocalsPath
 }
 
 /*!
@@ -36,29 +50,48 @@ var options = {
 var parameters = [
   { file: 'changelog.md', pattern: /[\d]{4}-[\d]{2}-[\d]{2}/ },
   { file: 'index.js', pattern: locals.package_description },
-  { file: 'package.json', pattern: format('"name": "%s"', locals.package_name) }
+  { file: 'package.json', pattern: format('"name": "%s"', locals.package_name) },
+  { file: 'readme.md', pattern: /tiny\('training'\)/ },
+  { file: 'license', pattern: locals.github_user_email },
+  { file: 'contributing.md', pattern: format('Contributing to %s', locals.package_name) },
+  { file: 'test.js', pattern: format('%s()', locals.package_name) },
+  { file: '.travis.yml', pattern: 'language: node_js' }
 ]
+
+/*!
+ * parameterized tests.
+ */
+
+function parameterizedTests (t) {
+  parameters.forEach(function (p) {
+    var file = path.resolve(projectCreationDir, p.file)
+    t.assert(fs.readFileSync(file, 'utf8').match(p.pattern), p.file)
+  })
+}
+
+/*!
+ * init.
+ */
+
+function init (t, sprout) {
+  sprout
+  .init(TEMPLATE_NAME, projectCreationDir, options)
+  .then(parameterizedTests.bind(null, t))
+}
 
 /*!
  * tests.
  */
 
-test('sprout-commonjs-lib', function (t) {
+test(TEMPLATE_NAME + 'content', function (t) {
   t.plan(parameters.length)
 
+  rimraf(projectCreationDir)
+  mkdirp(templateInstallDir)
+  var sprout = new Sprout(templateInstallDir)
+
   sprout
-    .add('commonjs-lib', template_path)
-    .then(function () {
-      rimraf.sync(test_project_path)
-    })
-    .then(function () {
-      sprout.init('commonjs-lib', test_project_path, options).then(function () {
-        parameters.forEach(function (p) {
-          var file = path.resolve(test_project_path, p.file)
-          t.assert(fs.readFileSync(file, 'utf8').match(p.pattern), p.file)
-        })
-      })
-    })
-    .done(function () {
-    })
+  .add(TEMPLATE_NAME, templateSourceRoot)
+  .then(init.bind(null, t, sprout))
+  .done()
 })
