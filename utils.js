48ae04213e81c8c4b3c0f7f30f2828d1e8dc949f
Wil Moore III <wil.moore@wilmoore.com>
2015-04-28 18:05:38 -0600

    extract helper utils into own module

---
+++ utils.js
@@ -0,0 +1,102 @@
+'use strict'
+
+/*!
+ * imports.
+ */
+
+var exec = require('child_process').execSync
+
+/*!
+ * exports.
+ */
+
+exports.exportName = exportName
+exports.gitConfigGet = gitConfigGet
+exports.repoName = repoName
+exports.simpleName = simpleName
+
+/**
+ * Default module export name (camelCased package name).
+ *
+ * @param {String} name
+ *
+ * @return {String}
+ * camelCased package name
+ */
+
+function exportName (name) {
+  return camel(simpleName(name))
+}
+
+/**
+ * Package name with file extension removed.
+ *
+ * @param {String} name
+ *
+ * @return {String}
+ * CamelCased Package name with file extension removed.
+ */
+
+function simpleName (name) {
+  return name.split(/^node-/).pop().split('.').shift()
+}
+
+/**
+ * Normalize repo name
+ *
+ * @param {String} name
+ * repository name.
+ *
+ * @return {String}
+ * normalized repository name.
+ */
+
+function repoName (name) {
+  // in case user inputs user/repo, we just take `repo` part.
+  return name.split('/').pop()
+}
+
+/**
+ * Get git config for given key.
+ *
+ * @param {String} key
+ *
+ * @return {String}
+ * Git configuration value.
+ */
+
+function gitConfigGet (key) {
+  return exec('git config --get ' + key).toString().trim()
+}
+
+/**
+ * CamelCase name.
+ *
+ * @param {String} name
+ *
+ * @return {String}
+ * CamelCased name.
+ */
+
+function camel (name) {
+  return name
+    .split(/[-_]/)
+    .map(uppercaseTail)
+    .join('')
+}
+
+/**
+ * UpperCase strings where `idx` > 0.
+ *
+ * @param {String} str
+ * @param {String} idx
+ *
+ * @return {String}
+ * Uppercase or un-modified string.
+ */
+
+function uppercaseTail (str, idx) {
+  return idx === 0
+    ? str
+    : str.replace(/^./, str[0].toUpperCase())
+}
